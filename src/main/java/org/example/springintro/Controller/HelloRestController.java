ab997f15e050e79ea141d04c6847c6f6c5280bf7
AbhayGoyal001 <abhaygoyalscience@gmail.com>
2025-03-01 16:51:44 +0530

    [Abhay Goyal] Added POST method

--- src/main/java/org/example/springintro/Controller/HelloRestController.java
+++ src/main/java/org/example/springintro/Controller/HelloRestController.java
@@ -15,24 +15,31 @@ public class HelloRestController {
     }
 
     // UC2 - Query Parameters: Handles GET requests at "/query" with a 'name' parameter.
-// URL: http://localhost:8080/query?name=Abhay
+// URL: http://localhost:8080/hello/query?name=Abhay
     @GetMapping("/query")
     public String sayHello(@RequestParam String name) {
         return "Hello " + name + " Thanks for using my application";
     }
 
     // UC3 - Path Parameters: Handles GET requests at "/{name}" using a path variable.
-// URL: http://localhost:8080/Abhay
+// URL: http://localhost:8080/hello/Abhay
     @GetMapping("/{name}")
     public String sayHelloPath(@PathVariable String name) {
         return "Hello " + name + " Thanks for using my application";
     }
 
     // UC4 - PostMethod: Handles POST requests at "/post" with a User object in the request body.
-// URL: Use POST request at http://localhost:8080/post with JSON body: { "firstName": "Abhay", "lastName": "Goyal" }
+// URL: Use POST request at http://localhost:8080/hello/post with JSON body: { "firstName": "Abhay", "lastName": "Goyal" }
     @PostMapping("/post")
     public String sayHelloPost(@RequestBody User user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " Thanks for using my application";
     }
+    // UC5 - PutMethod: Handles PUT requests at "/put" with a User object in the request body.
+// URL: Use PUT request at http://localhost:8080/put with JSON body: { "firstName": "Abhay", "lastName": "Goyal" }
+    @PutMapping("/put")
+    public String sayHelloPut(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " Thanks for using my application";
+    }
+
 
 }
