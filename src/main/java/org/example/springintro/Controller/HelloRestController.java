3b2c2318753ecde8337aa8b0aa3bb9a1fc771cdf
AbhayGoyal001 <abhaygoyalscience@gmail.com>
2025-03-01 14:39:42 +0530

    [Abhay Goyal] Added RequestParams

--- src/main/java/org/example/springintro/Controller/HelloRestController.java
+++ src/main/java/org/example/springintro/Controller/HelloRestController.java
@@ -1,13 +1,25 @@
 package org.example.springintro.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloRestController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+
+
+    // curl localhost:8080/hello -w "\n"
+    //uc1
+    @GetMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello Abhay this is my first spring boot application";
+
     }
 
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name+" Thanks for using my application";
+    }
 }
